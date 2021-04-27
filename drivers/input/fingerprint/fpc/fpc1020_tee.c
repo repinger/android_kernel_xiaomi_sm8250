20047bb72d6c5042c59c6a19ec595d1dfd820621
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-04-27 11:20:24 +0200

    drivers: Remove xiaomi early fingerprint wakeup optimization
    Change-Id: Ic732613398c48990a34de239682f3386ccc7e8c2
    Co-Authored-by: Dyneteve <dyneteve@hentaios.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: repinger <devel@repinger.my.id>

--- drivers/input/fingerprint/fpc/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_tee.c
@@ -55,7 +55,6 @@
 #define FPC_GPIO_NO_DEFINED -2
 #define FPC_GPIO_REQUEST_FAIL -3
 #define FPC_TTW_HOLD_TIME 1000
-#define FP_UNLOCK_REJECTION_TIMEOUT (FPC_TTW_HOLD_TIME - 500)
 
 #define RESET_LOW_SLEEP_MIN_US 5000
 #define RESET_LOW_SLEEP_MAX_US (RESET_LOW_SLEEP_MIN_US + 100)
@@ -805,7 +804,7 @@ static ssize_t irq_ack(struct device *dev, struct device_attribute *attr,
 {
 	struct fpc1020_data *fpc1020 = dev_get_drvdata(dev);
 
-	dev_dbg(fpc1020->dev, "%s\n", __func__);
+	dev_info(fpc1020->dev, "%s -> %s\n", __func__, buf);
 
 	return count;
 }
@@ -884,14 +883,6 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
-#ifndef FPC_DRM_INTERFACE_WA
-static void notification_work(struct work_struct *work)
-{
-	pr_info("%s: unblank\n", __func__);
-	dsi_bridge_interface_enable(FP_UNLOCK_REJECTION_TIMEOUT);
-}
-#endif
-
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
@@ -901,7 +892,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	mutex_lock(&fpc1020->lock);
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
 		fpc1020->nbr_irqs_received++;
-		__pm_wakeup_event(fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
+		__pm_wakeup_event(fpc1020->ttw_wl, (FPC_TTW_HOLD_TIME));
 	}
 	mutex_unlock(&fpc1020->lock);
 
@@ -1003,7 +994,6 @@ static int fpc1020_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	int rc = 0;
 	size_t i;
-
 	struct device_node *np = dev->of_node;
 	struct fpc1020_data *fpc1020 =
 		devm_kzalloc(dev, sizeof(*fpc1020), GFP_KERNEL);
@@ -1067,6 +1057,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 */
 	mutex_init(&fpc1020->lock);
 
+	mutex_init(&fpc1020->lock);
 	fpc1020->ttw_wl = wakeup_source_register(dev, "fpc_ttw_wl");
 	if (!fpc1020->ttw_wl)
 		return -ENOMEM;
